29e1322b9bc420407a9da2402d4a9aa8b9e5286e
Sean Darcy <darcys22@gmail.com>
2019-09-22 03:29:59 +0000

    transaction listing completed

--- reporter/main.go
+++ reporter/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/user"
 
 	"github.com/urfave/cli"
 )
 
-const (
-	defaultDBName = "~/.ledger/ledgerdata/ledger.db"
-)
+const ()
 
 var app *cli.App
 
 func init() {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defaultDBName = usr.HomeDir + "/.ledger/ledgerdata/ledger.db"
 	app = cli.NewApp()
 	app.Name = "Reporter"
 	app.Usage = "Provides GL and TB reports for GoDBLedger"
@@ -28,7 +33,8 @@ func init() {
 
 // Commonly used command line flags.
 var (
-	csvFlag = cli.BoolFlag{
+	defaultDBName = "/.ledger/ledgerdata/ledger.db"
+	csvFlag       = cli.BoolFlag{
 		Name:  "csv",
 		Usage: "output CSV instead of human-readable format",
 	}
--- reporter/transactionlisting.go
+++ reporter/transactionlisting.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
-	//"os"
-	//"path/filepath"
+	"log"
+	"os"
 
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
 )
+
+type Transaction struct {
+	Account     string `json:"account"`
+	ID          string `json:"id"`
+	Date        string `json:"date"`
+	Description string `json:"desc"`
+	Amount      string `json:"amount"`
+	Currency    string `json:"currency"`
+}
+
+var output struct {
+	Data []Transaction `json:"data"`
+}
 
 var commandTransactionListing = cli.Command{
 	Name:      "transactions",
@@ -30,17 +47,54 @@ If you want to see all the transactions in the database, or export to CSV
 		if databasefilepath == "" {
 			databasefilepath = defaultDBName
 		}
-		fmt.Println(databasefilepath)
-		//if _, err := os.Stat(databasefilepath); err != nil {
-		//panic(fmt.Sprintf("Database does not already exist at %s.", databasefilepath))
-		//}
+		if _, err := os.Stat(databasefilepath); err != nil {
+			panic(fmt.Sprintf("Database does not already exist at %s.", databasefilepath))
+		}
 
-		//fmt.Println("Success")
+		SqliteDB, err := sql.Open("sqlite3", databasefilepath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Account", "ID", "Date", "Description", "Currency", "Amount"})
+		table.SetBorder(false)
 
-		// Output some information.
-		//out := outputGenerate{
-		//Address: key.Address.Hex(),
-		//}
+		queryDB := `
+			SELECT 
+				splits.split_id,
+				splits.split_date,
+				splits.description,
+				splits.currency,
+				splits.amount,
+				split_accounts.account_id
+			FROM splits 
+				JOIN split_accounts 
+				ON splits.split_id = split_accounts.split_id
+		;`
+
+		rows, err := SqliteDB.Query(queryDB)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			// Scan one customer record
+			var t Transaction
+			if err := rows.Scan(&t.ID, &t.Date, &t.Description, &t.Currency, &t.Amount, &t.Account); err != nil {
+				// handle error
+			}
+			output.Data = append(output.Data, t)
+			table.Append([]string{t.Account, t.ID, t.Date, t.Description, t.Currency, t.Amount})
+		}
+		if rows.Err() != nil {
+			// handle error
+		}
+
+		//Output some information.
+		fmt.Println()
+		table.Render()
+		fmt.Println()
 		//if ctx.Bool(jsonFlag.Name) {
 		//mustPrintJSON(out)
 		//} else {
