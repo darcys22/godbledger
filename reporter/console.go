7aea5a24425473622db9b49f038a3083b02ae437
Sean Darcy <darcys22@gmail.com>
2019-11-05 09:44:12 +1100

    made the console for reporter work, no options yet

--- reporter/console.go
+++ reporter/console.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 
+	"github.com/mattn/go-colorable"
 	"github.com/urfave/cli"
 )
 
 const DefaultPrompt = "> "
 
+type Config struct {
+	DataDir string // Data directory to store the console history at
+	DocRoot string // Filesystem path from where to load JavaScript files from
+	//Client   *rpc.Client  // RPC client to execute Ethereum requests through
+	Prompt   string        // Input prompt prefix string (defaults to DefaultPrompt)
+	Prompter *bufio.Reader // Input prompter to allow interactive user feedback (defaults to TerminalPrompter)
+	Printer  io.Writer     // Output writer to serialize any display strings to (defaults to os.Stdout)
+	Preload  []string      // Absolute paths to JavaScript files to preload
+}
+
 type Console struct {
-	printer io.Writer // Output writer to serialize any display strings to
+	prompter *bufio.Reader // Input prompter to allow interactive user feedback (defaults to TerminalPrompter)
+	printer  io.Writer     // Output writer to serialize any display strings to
 }
 
 func New(config Config) (*Console, error) {
 	// Handle unset config values gracefully
-	config.Prompter = Stdin
+	config.Prompter = bufio.NewReader(os.Stdin)
+	config.Prompt = DefaultPrompt
 	config.Printer = colorable.NewColorableStdout()
 
 	// Initialize the console and return
@@ -22,6 +38,8 @@ func New(config Config) (*Console, error) {
 		prompter: config.Prompter,
 		printer:  config.Printer,
 	}
+
+	return console, nil
 }
 
 // Welcome show summary of current Database and some metadata about the
@@ -33,24 +51,40 @@ func (c *Console) Welcome() {
 	fmt.Fprintln(c.printer, message)
 }
 
+// Evaluate executes code and pretty prints the result to the specified output
+// stream.
+func (c *Console) Evaluate(statement string) error {
+
+	switch statement {
+	case "tb":
+		fmt.Println("Today is 5th. Clean your house.")
+	case "gl":
+		fmt.Println("Today is 5th. Clean your house.")
+	default:
+		fmt.Println("No information available for that day.")
+	}
+
+	return nil
+}
+
 // PromptInput displays the given prompt to the user and requests some textual
 // data to be entered, returning the input of the user.
 func (c *Console) PromptInput(prompt string) (string, error) {
-	if p.supported {
-		p.rawMode.ApplyMode()
-		defer p.normalMode.ApplyMode()
-	} else {
-		// liner tries to be smart about printing the prompt
-		// and doesn't print anything if input is redirected.
-		// Un-smart it by printing the prompt always.
-		fmt.Print(prompt)
-		prompt = ""
-		defer fmt.Println()
-	}
-	return p.State.Prompt(prompt)
+	fmt.Print(prompt)
+	prompt = ""
+	defer fmt.Println()
+
+	return c.prompter.ReadString('\n')
 }
 
 func reporterConsole(c *cli.Context) error {
-	fmt.Printf("Hello %q", c.Args().Get(0))
+
+	console, _ := New(Config{})
+	console.Welcome()
+
+	something, _ := console.PromptInput("Report to Run:")
+	fmt.Printf("Hello %s", something)
+	console.Evaluate(something)
+
 	return nil
 }
