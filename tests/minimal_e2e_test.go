f53284ba30d8e89b2a776ed6439abc8c57fc10e1
Sean Darcy <darcys22@gmail.com>
2020-07-12 12:40:04 +0000

    passing initial test

---
+++ tests/minimal_e2e_test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/darcys22/godbledger/godbledger/cmd"
+)
+
+func TestEndToEnd_MinimalConfig(t *testing.T) {
+
+	set := flag.NewFlagSet("test", 0)
+	set.String("config", "", "doc")
+
+	ctx := cli.NewContext(nil, set, nil)
+
+	err, cfg := cmd.MakeConfig(ctx)
+	if err != nil {
+		t.Fatalf("New Config Failed: %v", err)
+	}
+
+	cfg.DatabaseType = "sqlite3"
+	cfg.DatabaseLocation = ":memory:"
+
+	runEndToEndTest(t, cfg)
+}
