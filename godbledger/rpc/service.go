2b342b332d2dedfef2bc7535dfab414f3c98d619
Sean Darcy <darcys22@gmail.com>
2020-08-04 12:13:56 +0000

    Added GRPC security to endpoints
    GoDBLedger will eventually need to be run on a server with ports open
    to the public, this endpoint will need to be secure and GRPC has the
    ability to lock down the with mutual TLC

--- godbledger/rpc/service.go
+++ godbledger/rpc/service.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
 
 	"github.com/darcys22/godbledger/godbledger/ledger"
 	pb "github.com/darcys22/godbledger/proto"
@@ -19,28 +21,35 @@ func init() {
 }
 
 type Service struct {
-	ld         *ledger.Ledger
-	ctx        context.Context
-	cancel     context.CancelFunc
-	grpcServer *grpc.Server
-	listener   net.Listener
-	port       string
-	host       string
+	ld              *ledger.Ledger
+	ctx             context.Context
+	cancel          context.CancelFunc
+	grpcServer      *grpc.Server
+	listener        net.Listener
+	port            string
+	host            string
+	withCert        string
+	withKey         string
+	credentialError error
 }
 
 type Config struct {
-	Port string
-	Host string
+	Port     string
+	Host     string
+	CertFlag string
+	KeyFlag  string
 }
 
 func NewRPCService(ctx context.Context, cfg *Config, l *ledger.Ledger) *Service {
 	ctx, cancel := context.WithCancel(ctx)
 	return &Service{
-		ld:     l,
-		ctx:    ctx,
-		cancel: cancel,
-		port:   cfg.Port,
-		host:   cfg.Host,
+		ld:       l,
+		ctx:      ctx,
+		cancel:   cancel,
+		port:     cfg.Port,
+		host:     cfg.Host,
+		withCert: cfg.CertFlag,
+		withKey:  cfg.KeyFlag,
 	}
 }
 
@@ -53,12 +62,32 @@ func (s *Service) Start() {
 		log.Errorf("Could not listen to port in Start() %s: %v", address, err)
 	}
 	s.listener = lis
-	log.WithField("port", s.port).Info("Listening on port")
-	s.grpcServer = grpc.NewServer()
+	log.WithField("address", address).Info("GRPC Listening on port")
+
+	opts := []grpc.ServerOption{}
+
+	if s.withCert != "" && s.withKey != "" {
+		creds, err := credentials.NewServerTLSFromFile(s.withCert, s.withKey)
+		if err != nil {
+			log.Errorf("Could not load TLS keys: %s", err)
+			s.credentialError = err
+		}
+		opts = append(opts, grpc.Creds(creds))
+	} else {
+		log.Warn("You are using an insecure gRPC server. If you are running your GoDBLedger Server and " +
+			"client on the same machine, you can ignore this message.")
+		//"client on the same machine, you can ignore this message. If you want to know " +
+		//"how to enable secure connections, see: https://docs.prylabs.network/docs/prysm-usage/secure-grpc")
+	}
+
+	s.grpcServer = grpc.NewServer(opts...)
 
 	ledgerServer := &LedgerServer{ld: s.ld}
 
 	pb.RegisterTransactorServer(s.grpcServer, ledgerServer)
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s.grpcServer)
 
 	go func() {
 		if s.listener != nil {
@@ -82,5 +111,8 @@ func (s *Service) Stop() error {
 
 // Status returns nil or credentialError
 func (s *Service) Status() error {
+	if s.credentialError != nil {
+		return s.credentialError
+	}
 	return nil
 }
