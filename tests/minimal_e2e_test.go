63c7b031115827bcf7ecac39647c443c998cd5e6
David Alpert <david@spinthemoose.com>
2020-11-01 17:21:49 -0600

    update e2e tests to use ./release/test-data as a local data dir
    - this keeps config files generated for the e2e tests separate
      from any user-specific config files allowing contributors to
      keep local e2e test runs separate from installed and in-use
      configurations
    
    - explicitly remove and regenerate test config on each e2e test
      run so that they are consistent
    
    - explicitly remove any old test data file on each e2e test run
      so that they are consistent
    
    - this ensures that e2e test runs are deterministic wrt config
      and initial database state
    
    - log binPath as well as args to add context when reading the
      logs

--- tests/minimal_e2e_test.go
+++ tests/minimal_e2e_test.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	"flag"
+	"os"
+	"path"
+	"path/filepath"
 	"testing"
 
 	"github.com/urfave/cli/v2"
@@ -15,7 +18,33 @@ func TestEndToEnd_MinimalConfig(t *testing.T) {
 
 	// Create a config from the defaults which would usually be created by the CLI library
 	set := flag.NewFlagSet("test", 0)
-	set.String("config", "", "doc")
+	set.String("datadir", "", "data directory")
+	set.String("config", "", "config file")
+	set.String("database-location", "", "database location")
+
+	testDataDir, err := filepath.Abs("../release/test-data")
+	if err != nil {
+		t.Fatalf("Calculating path to test data dir: %v", err)
+	}
+	testConfigFile := path.Join(testDataDir, "config.toml")
+	if fileExists(testConfigFile) {
+		err := os.Remove(testConfigFile)
+		if err != nil {
+			t.Fatalf("Removing old test Config file failed: %v", err)
+		}
+	}
+	testDatabaseLocation := path.Join(testDataDir, "ledger.db")
+	if fileExists(testDatabaseLocation) {
+		err := os.Remove(testDatabaseLocation)
+		if err != nil {
+			t.Fatalf("Removing old test database file failed: %v", err)
+		}
+	}
+
+	set.Set("datadir", testDataDir)
+	set.Set("config", testConfigFile)
+	set.Set("config", testDatabaseLocation)
+
 	ctx := cli.NewContext(nil, set, nil)
 	err, cfg := cmd.MakeConfig(ctx)
 	if err != nil {
@@ -30,4 +59,12 @@ func TestEndToEnd_MinimalConfig(t *testing.T) {
 		t.Fatal(err)
 	}
 	runEndToEndTest(t, cfg)
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
