c0e2cdf01349d42a0eba5d83fed256e9a099e116
Sean Darcy <darcys22@gmail.com>
2020-12-29 18:35:00 +1100

    uses utils/gen.sh to build certificates and keys and then tests a connection using them

--- tests/secure_connection_test.go
+++ tests/secure_connection_test.go
@@ -6,9 +6,13 @@ package tests
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"strconv"
 	"testing"
 	"time"
@@ -22,10 +26,10 @@ import (
 
 	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func secure_connection_test(t *testing.T) {
-
 	// Create a config from the defaults which would usually be created by the CLI library
 	set := flag.NewFlagSet("test", 0)
 	set.String("config", "", "doc")
@@ -40,6 +44,22 @@ func secure_connection_test(t *testing.T) {
 
 	// Set the RPC port to random higher port to not clash with other tests
 	cfg.RPCPort = "55051"
+
+	// Start by generating the Certificates and Private Keys necessary for both the server and the client.
+	// This is done by a bash script in the utils directory
+	cmd := exec.Command("../utils/gen.sh")
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("Failed Generating Certificates and Keys: %v", err)
+	}
+
+	// Add the servers credential filenames to the configuration
+	cfg.CACert = "ca-cert.pem"
+	cfg.Cert = "server-cert.pem"
+	cfg.Key = "server-key.pem"
+
+	// Also add the clients credentials to variables for later usage
+	clientCertFilename := "client-cert.pem"
+	clientKeyFilename := "client-key.pem"
 
 	processIDs := []int{}
 	logFiles := []*os.File{}
@@ -69,36 +89,33 @@ func secure_connection_test(t *testing.T) {
 	t.Logf("Starting GoDBLedger")
 	port, _ := strconv.Atoi(cfg.RPCPort)
 
-	opts := []grpc.ServerOption{}
-	if s.withCACert != "" && s.withCert != "" && s.withKey != "" {
-		// Load certificate of the CA who signed client's certificate
-		pemClientCA, err := ioutil.ReadFile(s.withCACert)
-		if err != nil {
-			return nil, err
-		}
-		certPool := x509.NewCertPool()
-		if !certPool.AppendCertsFromPEM(pemClientCA) {
-			return nil, fmt.Errorf("failed to add client CA's certificate")
-		}
+	opts := []grpc.DialOption{}
 
-		// Load server's certificate and private key
-		serverCert, err := tls.LoadX509KeyPair(s.withCert, s.withKey)
-		if err != nil {
-			return nil, err
-		}
-
-		// Create the credentials and return it
-		config := &tls.Config{
-			Certificates: []tls.Certificate{serverCert},
-			ClientAuth:   tls.RequireAndVerifyClientCert,
-			ClientCAs:    certPool,
-		}
-
-		creds := credentials.NewTLS(config)
-		opts = append(opts, grpc.Creds(creds))
-	} else {
-		t.Fatal("You are using an insecure gRPC server. This test was designed to test a secure connection")
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile(cfg.CACert)
+	if err != nil {
+		t.Fatalf("Failed reading CA certificate: %v", err)
 	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		t.Fatal("failed to add CA's certificate to pool")
+	}
+
+	// Load client's certificate and private key
+	clientCert, err := tls.LoadX509KeyPair(clientCertFilename, clientKeyFilename)
+	if err != nil {
+		t.Fatalf("Failed reading Client certificate and key: %v", err)
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	tlsCredentials := credentials.NewTLS(config)
+	opts = append(opts, grpc.WithTransportCredentials(tlsCredentials))
 
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", cfg.Host, port), opts...)
 	if err != nil {
