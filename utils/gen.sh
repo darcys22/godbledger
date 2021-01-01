a5c0c370b78335b066ad4d4fca52ad37b370d6b2
Sean Darcy <darcys22@gmail.com>
2021-01-01 19:13:31 +1100

    longer lasting certs

--- utils/gen.sh
+++ utils/gen.sh
@@ -4,7 +4,7 @@ export IP="127.0.0.1"
 rm *.pem
 
 # 1. Generate CA's private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=AU/ST=Victoria/L=Melbourne/O=Darcy Financial/OU=GoDBLedger/CN=*.darcyfinancial.com/emailAddress=sean@darcyfinancial.com"
+openssl req -x509 -newkey rsa:4096 -days 3650 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=AU/ST=Victoria/L=Melbourne/O=Darcy Financial/OU=GoDBLedger/CN=*.darcyfinancial.com/emailAddress=sean@darcyfinancial.com"
 
 echo "CA's self-signed certificate"
 openssl x509 -in ca-cert.pem -noout -text
@@ -14,7 +14,7 @@ openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
 echo subjectAltName = IP:${IP} > server-ext.cnf
-openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+openssl x509 -req -in server-req.pem -days 3650 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
 echo "Server's signed certificate"
 openssl x509 -in server-cert.pem -noout -text
@@ -24,6 +24,6 @@ openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -
 
 # 5. Use CA's private key to sign client's CSR and get back the signed certificate
 echo subjectAltName = IP:${IP} > client-ext.cnf
-openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
+openssl x509 -req -in client-req.pem -days 3650 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
 echo "Client's signed certificate"
 openssl x509 -in client-cert.pem -noout -text
