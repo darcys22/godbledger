1a1ab7ef3cb6f47c7e8e8cde9e7d3dc2525aec09
Sean Darcy <darcys22@gmail.com>
2021-05-02 15:54:49 +1000

    include buildinfo

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#go run utils/ci.go debsrc -upload darcys22/godbledger -sftp-user darcys22 -signer "Sean Darcy <sean@              darcyfinancial.com>"
-go run utils/ci.go debsrc -upload darcys22/godbledger -sftp-user darcys22 -signer "Sean Darcy <sean@darcyfinancial.com>"
