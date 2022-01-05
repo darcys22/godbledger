9e205cd036b8a1008cdcd347603c7158539c9e32
Sean Darcy <darcys22@gmail.com>
2022-01-06 09:28:47 +1100

    Bump to v0.7.0

--- godbledger/version/version.go
+++ godbledger/version/version.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	VersionMajor = 0       // Major version component of the current release
-	VersionMinor = 6       // Minor version component of the current release
+	VersionMinor = 7       // Minor version component of the current release
 	VersionPatch = 0       // Patch version component of the current release
 	VersionMeta  = "alpha" // Version metadata to append to the version string
 )
